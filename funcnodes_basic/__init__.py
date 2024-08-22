28946b3c124f8e9b70a2aa3d979ffef8e57e6f22
Julian Kimmig <julian.kimmig@gmx.net>
2024-08-22 16:00:34 +0200

    version attribute

--- funcnodes_basic/__init__.py
+++ funcnodes_basic/__init__.py
@@ -4,6 +4,8 @@ from .math import NODE_SHELF as math_shelf
 from .lists import NODE_SHELF as lists_shelf
 from .strings import NODE_SHELF as strings_shelf
 
+__version__ = "0.1.0"
+
 NODE_SHELF = Shelf(
     nodes=[],
     subshelves=[
