d220d93e716b269cff720aecf587536995dcbfab
JulianKimmig <julian.kimmig@gmx.net>
2025-06-04 12:56:27 +0200

    bump version to 0.2.3

--- src/funcnodes_basic/__init__.py
+++ src/funcnodes_basic/__init__.py
@@ -7,7 +7,7 @@ from .dicts import NODE_SHELF as dicts_shelf
 from .input import NODE_SHELF as input_shelf
 from .dataclass import NODE_SHELF as dataclass_shelf
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 NODE_SHELF = Shelf(
     nodes=[],
